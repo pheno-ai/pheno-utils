085aede574160246d290d1d578379d4f69ef0a2c
alondmnt <alondmnt@users.noreply.github.com>
2023-04-23 14:08:04 +0300

    improved: imports

--- pheno_utils/__init__.py
+++ pheno_utils/__init__.py
@@ -1,1 +1,12 @@
 __version__ = "0.1.4"
+
+from .age_reference_plots import *
+from .basic_analysis import *
+from .basic_plots import *
+from .blandaltman_plots import *
+from .cgm_plots import *
+from .config import *
+from .data_loader import DataLoader as PhenoLoader
+from .dates_plots import *
+from .ecg_analysis import *
+from .sleep_plots import *
